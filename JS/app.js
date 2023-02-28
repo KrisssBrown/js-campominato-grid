03421cc6487860cbcd953249875df7c44f418915
KrisssBrown <cristianbruno.cs@gmail.com>
2023-02-28 15:54:11 +0100

    FEAT first component

--- JS/app.js
+++ JS/app.js
@@ -2,19 +2,18 @@
 
 
 const btnEl = document.getElementById('play-button')
-const colInput = document.getElementById ('col-input')
-const rowInput = document.getElementById ('row-input')
-const gridEl = document.getElementById ('grid')
+const colInput = document.getElementById('col-input')
+const rowInput = document.getElementById('row-input')
+const gridEl = document.getElementById('grid')
+
+btnEl.addEventListener('click', startGame)
 
 
-
-
-btnEl.addEventListener('click', function () {
-
-    btnEl.classList.add ('d-none')
-    colInput.classList.add ('d-none')
-    rowInput.classList.add ('d-none')
-    gridEl.classList.remove ('flex-column', 'gap-3')
+function startGame() {
+    // btnEl.classList.add('d-none')
+    // colInput.classList.add('d-none')
+    // rowInput.classList.add('d-none')
+    gridEl.classList.remove('flex-column', 'gap-3')
 
     let colonne = colInput.value
     let righe = rowInput.value
@@ -45,7 +44,47 @@ btnEl.addEventListener('click', function () {
 
     }
 
-})
+}
+
+
+
+// // // // // btnEl.addEventListener('click', function () {
+
+// // // // //     btnEl.classList.add('d-none')
+// // // // //     colInput.classList.add('d-none')
+// // // // //     rowInput.classList.add('d-none')
+// // // // //     gridEl.classList.remove('flex-column', 'gap-3')
+
+// // // // //     let colonne = colInput.value
+// // // // //     let righe = rowInput.value
+// // // // //     // let colonne = parseInt(prompt('Inserisci il numero di colonne!'))
+// // // // //     // let righe = parseInt(prompt('Inserisci il numero di righe!'))
+// // // // //     let numeroCelle = colonne * righe
+
+// // // // //     console.log(colonne, righe, numeroCelle)
+
+// // // // //     const grigliaEl = document.getElementById('grid')
+// // // // //     console.log(grigliaEl)
+
+// // // // //     for (let i = 0; i < numeroCelle; i++) {
+// // // // //         let num = i + 1
+// // // // //         console.log(num)
+
+// // // // //         let cellaEl = document.createElement('div')
+// // // // //         cellaEl.innerHTML = num
+// // // // //         cellaEl.classList.add('cella')
+// // // // //         grigliaEl.append(cellaEl)
+
+// // // // //         // CELLAELEMENT STYLE
+// // // // //         cellaEl.style.width = 'calc(100% / ' + colonne + ')'
+// // // // //         cellaEl.style.height = 'calc(100% / ' + righe + ')'
+
+
+// // // // //         console.log(cellaEl)
+
+// // // // //     }
+
+// // // // // })
 
 // console.log(cellaEl)
 
